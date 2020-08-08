d6173d7c5fd27ef77ecb403b6e99f8ea050abfe5
Rian Marlon <rianmarlon20@yahoo.com>
2020-08-08 20:58:21 -0300

    Adicionando o estilo do componente TeacherItem da pasta components

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,25 +1,51 @@
 import React from 'react';
 import { View, Image, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import heartOutlineIcon from '../../assets/images/icons/heart-outline.png';
+import unfavoriteIcon from '../../assets/images/icons/unfavorite.png';
+import whatsappIcon from '../../assets/images/icons/whatsapp.png';
+
+import styles from './styles';
 
 function TeacherItem() {
   return (
-    <View>
-      <View>
+    <View style={styles.container}>
+      <View style={styles.profile}>
         <Image 
           source={{ uri: 'https://github.com/RianMarlon.png' }}
+          style={styles.avatar}
         />
 
-        <View>
-          <Text>Rian Marlon</Text>
-          <Text>Matemática</Text>
+        <View style={styles.profileInfo}>
+          <Text style={styles.name}>Rian Marlon</Text>
+          <Text style={styles.subject}>Matemática</Text>
         </View>
       </View>
 
-      <Text>
+      <Text style={styles.biography}>
         Entuasiasta nas melhores técnicas de cálculos matemáticos do mundo.
         {'\n'}{'\n'}
-        Considerado um dos melhores professores de Matemática do mundo, sendo reconhecido por vários cientistas como o Leonard Euler II.
+        Considerado um dos melhores professores de Matemática do mundo. Reconhecido por vários cientistas como o Leonard Euler II.
       </Text>
+
+      <View style={styles.footer}>
+        <Text style={styles.price}>
+          Preço/hora {'   '} 
+          <Text style={styles.priceValue}>R$300,00</Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+          <RectButton style={styles.favoriteButton}>
+            <Image source={heartOutlineIcon} />
+          </RectButton>
+
+          <RectButton style={styles.contactButton}>
+            <Image source={whatsappIcon} />
+            <Text style={styles.contactButtonText}>Entar em contato</Text>
+          </RectButton>
+        </View>
+      </View>
     </View>
   );
 }
