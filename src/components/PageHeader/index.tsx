82dbbc0918414b562f574b0f645c1c059a88febe
Rian Marlon <rianmarlon20@yahoo.com>
2020-08-08 01:50:10 -0300

    Adicionando a estrutura do componente PageHeader da pasta components

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+
+import backIcon from '../../assets/images/icons/back.png';
+import logoImg from '../../assets/images/logo.png';
 
 function PageHeader() {
-  return <View />
+
+  function handleGoBack() {
+
+  }
+
+  return (
+    <View>
+      <View>
+        <BorderlessButton onPress={handleGoBack}>
+          <Image source={backIcon} resizeMode="contain" />
+        </BorderlessButton>
+
+        <Image source={logoImg} resizeMode="contain" />
+      </View>
+    </View>
+  );
 }
 
 export default PageHeader;
