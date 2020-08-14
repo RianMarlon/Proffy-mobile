69cf4ce78a384080bff6a9765e9ada358aba80bc
Rian Marlon <rianmarlon20@yahoo.com>
2020-08-14 17:32:44 -0300

    Alterando o estilo do componente PageHeader

--- src/components/PageHeader/styles.ts
+++ src/components/PageHeader/styles.ts
@@ -2,20 +2,38 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    padding: 40,
     backgroundColor: '#8257E5',
+    paddingBottom: 40,
   },
 
   topBar: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+    paddingHorizontal: 40,
+    paddingTop: 25,
+    paddingBottom: 5,
+    backgroundColor: '#774DD6',
+  },
+
+  buttonBack: {
+    padding: 10,
+    marginLeft: -10,
+    marginRight: -10,
+  },
+
+  namePage: {
+    fontFamily: 'Archivo_500Medium',
+    fontSize: 14,
+    lineHeight: 15,
+    color: '#D4C2FF',
   },
 
   header: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+    paddingHorizontal: 40,
   },
 
   title: {
@@ -26,6 +44,11 @@ const styles = StyleSheet.create({
     maxWidth: 160,
     marginVertical: 40,
   },
+
+  children: {
+    paddingHorizontal: 40,
+  },
+
 });
 
 export default styles;
