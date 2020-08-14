3254a4cab6d199a0e7868acb76edeb64d58c980f
Rian Marlon <rianmarlon20@yahoo.com>
2020-08-14 17:21:16 -0300

    Adicionando a fonte Archivo 500 Medium no App.tsx

--- App.tsx
+++ App.tsx
@@ -3,12 +3,13 @@ import React from 'react';
 import AppStack from './src/routes/AppStack';
 
 import { AppLoading } from 'expo';
-import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Archivo_400Regular, Archivo_700Bold, Archivo_500Medium, useFonts } from '@expo-google-fonts/archivo';
 import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
 
 export default function App() {
   let [fontsLoaded] = useFonts({
     Archivo_400Regular,
+    Archivo_500Medium,
     Archivo_700Bold,
     Poppins_400Regular,
     Poppins_600SemiBold
