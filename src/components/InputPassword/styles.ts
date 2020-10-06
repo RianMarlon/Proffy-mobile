09e4282d2bb5a465890a8b80407f6dfea806e83b
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-06 19:51:52 -0300

    Adicionando o estilo do componente InputPassword da pasta components

---
+++ src/components/InputPassword/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  buttonShowPassword: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'transparent',
+    borderWidth: 0,
+  },
+
+});
+
+export default styles;
