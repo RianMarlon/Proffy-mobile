a41152b43eb27bd90681d783f528d856e6538210
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-05 21:35:44 -0300

    Adicionando o estilo do componente Navbar da pasta components

---
+++ src/components/Navbar/styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  topBar: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 40,
+    paddingTop: 25,
+    paddingBottom: 5,
+    backgroundColor: '#774DD6',
+  },
+
+  buttonBack: {
+    padding: 10,
+    marginLeft: -10,
+    marginRight: -10,
+  },
+
+  namePage: {
+    fontFamily: 'Archivo_500Medium',
+    fontSize: 14,
+    lineHeight: 15,
+    color: '#D4C2FF',
+  },
+
+});
+
+export default styles;
