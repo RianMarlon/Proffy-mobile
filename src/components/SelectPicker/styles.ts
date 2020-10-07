f5c206e03b5b7a07fd35d963c1989206684ea18d
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-07 14:13:22 -0300

    Alterando o estilo do componente SelectPicker da pasta components

--- src/components/SelectPicker/styles.ts
+++ src/components/SelectPicker/styles.ts
@@ -2,19 +2,34 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   label: {
-    color: '#D4C2FF',
+    color: '#9C98A6',
     fontFamily: 'Poppins_400Regular',
+    fontSize: 14,
+    lineHeight: 22,
+  },
+
+  error: {
+    color: '#E83F5B',
   },
 
   picker: {
-    height: 54,
-    backgroundColor: '#FFF',
+    height: 56,
+    marginTop: 4,
+    backgroundColor: '#FAFAFC',
+    borderWidth: 1,
+    borderColor: '#E6E6F0',
     borderRadius: 8,
     justifyContent: 'center',
-    paddingHorizontal: 8,
-    marginTop: 4,
-    marginBottom: 16,
+    paddingHorizontal: 10,
   },
-})
+
+  itemPicker: {
+    color: '#6A6180',
+    fontFamily: 'Poppins_400Regular',
+    fontSize: 14,
+    lineHeight: 24,
+  },
+  
+});
 
 export default styles;
