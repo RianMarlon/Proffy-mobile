53c6c8524716b81311399f5f9f9bd7be62321d79
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-07 14:01:06 -0300

    Alterando a estrutura e adicionando o componente Navbar no componente PageHeader da pasta components

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,49 +1,49 @@
 import React, { ReactNode } from 'react';
-import { View, Image, Text } from 'react-native';
-import { BorderlessButton } from 'react-native-gesture-handler';
-
-import { useNavigation } from '@react-navigation/native';
-
-import backIcon from '../../assets/images/icons/back.png';
-import logoImg from '../../assets/images/logo.png';
+import { View, Text } from 'react-native';
 
 import styles from './styles';
+import Navbar from '../Navbar';
 
 interface PageHeaderProps {
-  namePage?: string
+  namePage: string
   title: string,
+  description?: string,
   headerRight?: ReactNode,
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ namePage, title, headerRight, children }) => {
-  const { navigate } = useNavigation();
-
-  function handleGoBack() {
-    navigate('Landing');
-  }
+const PageHeader: React.FC<PageHeaderProps> = ({
+    namePage, title, description, headerRight, children
+  }) => {
 
   return (
     <View style={styles.container}>
-      <View style={styles.topBar}>
-        <BorderlessButton style={styles.buttonBack} onPress={handleGoBack}>
-          <Image source={backIcon} resizeMode="contain" />
-        </BorderlessButton>
+      <Navbar
+        namePage={namePage}
+      />
 
-        <Text style={styles.namePage}>{namePage}</Text>
-
-        <Image source={logoImg} resizeMode="contain" />
-      </View>
-
-      <View style={styles.header}>
-        <Text style={styles.title}>{title}</Text>
-        { headerRight && headerRight}
-      </View>
-
-      { children && (
-        <View style={styles.children}>
-          {children}
+      <View style={styles.headerContent}>
+        <View>
+          <Text style={headerRight 
+            ? styles.title 
+            : [styles.title, styles.noHeaderRight]
+          }>
+            {title}
+          </Text>
+          {description && 
+            <Text style={styles.description}>
+              {description}
+            </Text>
+          }
         </View>
-      )}
+        { headerRight && (
+          <View>
+            { headerRight }
+          </View>
+        )}
+      </View>
+      <View style={styles.children}>
+        {children}
+      </View>
     </View>
   );
 }
