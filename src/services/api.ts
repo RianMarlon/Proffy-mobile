981021bef21a4960efd336f281c22253a9e65fc2
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-08 14:23:25 -0300

    Alterando o arquivo api.ts da pasta services

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,18 @@
-import axios from 'axios';
+import AsyncStorage from '@react-native-community/async-storage';
+import axios, { AxiosRequestConfig } from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.5:3333'
+  baseURL: 'http://192.168.1.6:3333',
+});
+
+api.interceptors.request.use(async (config: AxiosRequestConfig) => {
+  const token = await AsyncStorage.getItem('@proffy/user');
+  
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default api;
