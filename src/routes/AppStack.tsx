5e626b22590539ca4dcb3883b4bb7774b599cf22
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-09 13:44:15 -0300

    Adicionando a tela de Login nas navegações do arquivo AppStack.tsx

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+import Login from '../pages/Login';
 import Landing from '../pages/Landing';
-import GiveClasses from '../pages/GiveClasses';
 import StudyTabs from './StudyTabs';
+import GiveClasses from '../pages/GiveClasses';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -12,12 +13,13 @@ function AppStack() {
   return (
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
+        <Screen name="Login" component={Login} />
         <Screen name="Landing" component={Landing} />
         <Screen name="GiveClasses" component={GiveClasses} />
         <Screen name="Study" component={StudyTabs} />
       </Navigator>
     </NavigationContainer>
-  )
+  );
 }
 
 export default AppStack;
