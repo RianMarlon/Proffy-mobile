cd406aac24902bbbfc33226bde22d9daac24da7f
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-11 01:53:12 -0300

    Adicionando o RootSiblingParent no App.tsx

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import AppStack from './src/routes/AppStack';
 
+import { RootSiblingParent } from 'react-native-root-siblings';
+
 import { AppLoading } from 'expo';
 import { Archivo_400Regular, Archivo_600SemiBold, Archivo_700Bold, Archivo_500Medium, useFonts } from '@expo-google-fonts/archivo';
 import { Poppins_400Regular, Poppins_600SemiBold, Poppins_500Medium } from '@expo-google-fonts/poppins';
@@ -23,10 +25,10 @@ export default function App() {
 
   else {
     return (
-      <>
+      <RootSiblingParent>
         <AppStack />
         <StatusBar style="light" />
-      </>
+      </RootSiblingParent>
     );
   }
 }
