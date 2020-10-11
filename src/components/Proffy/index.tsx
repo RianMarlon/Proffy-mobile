8fa70877621975e0e9f387b3e6b360379dc5fa84
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-11 17:03:35 -0300

    Alterando a estrutura do componente Proffy da pasta components

--- src/components/Proffy/index.tsx
+++ src/components/Proffy/index.tsx
@@ -2,15 +2,15 @@ import React  from 'react';
 import { Text, View, Image, ImageBackground } from 'react-native';
 
 import proffyLogoBigger from '../../assets/images/logo-bigger.png';
-import successBackground from '../../assets/images/success-background.png';
+import introBackground from '../../assets/images/intro-background.png';
 
 import styles from './styles';
 
 const Proffy: React.FC = () => {
   return (
     <View style={styles.proffyContainer}>
-      <ImageBackground resizeMode="repeat"
-        source={successBackground} 
+      <ImageBackground resizeMode="contain"
+        source={introBackground} 
         style={styles.proffyBackgroundImage}
       >
         <View style={styles.content}>
