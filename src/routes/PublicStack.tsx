a12c735e13ac4dcca2bffdbd6873bc8f7b4c64ea
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-11 11:01:22 -0300

    Adicionando o arquivo PublicStack.tsx na pasta routes

---
+++ src/routes/PublicStack.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Login from '../pages/Login';
+import Register from '../pages/Register';
+import ForgotPassword from '../pages/ForgotPassword';
+
+const { Navigator, Screen } = createStackNavigator();
+
+function PublicStack() {
+  return (
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Screen name="Login" component={Login} />
+      <Screen name="Register" component={Register} />
+      <Screen name="ForgotPassword" component={ForgotPassword} />
+    </Navigator>
+  );
+}
+
+export default PublicStack;
