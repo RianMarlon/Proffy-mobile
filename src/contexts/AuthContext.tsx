e5102c5a51d044721d4f8c90b4df880f36cb5708
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-13 11:48:04 -0300

    Adicionando o ActivityIndicator no AuthContext da pasta contexts

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,5 @@
 import React, { createContext, useEffect, useState } from 'react';
+import { View, ActivityIndicator } from 'react-native';
 import { hasTokenValid } from '../services/auth';
 
 interface AuthContextData {
@@ -10,6 +11,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
   const [isValidToken, setIsValidToken] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     checkToken();
@@ -17,8 +19,21 @@ export const AuthProvider: React.FC = ({ children }) => {
 
   async function checkToken() {
     const response = await hasTokenValid();
-  
+    
+    setLoading(false);
     setIsValidToken(response);
+  }
+
+  if (loading) {
+    return (
+      <View style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center' 
+      }}>
+        <ActivityIndicator size={80} color="#8257E5" />
+      </View>
+    );
   }
 
   return (
